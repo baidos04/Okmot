81b0dadb5bac19e777f8a638c3565add36710e78
baidos04 <muslimovbaidoskii@gmail.com>
2023-07-11 08:36:35 +0600

    news commit

--- newsapp/models.py
+++ newsapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+from django.db import models
+
+class News(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    image = models.ImageField(upload_to='news/images/', null=True, blank=True)
+    video = models.FileField(upload_to='news/videos/', null=True, blank=True)
+    published = models.BooleanField(default=False)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-published', '-time_create']
--- newsapp/views.py
+++ newsapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from .models import News
+from .serializers import *
+
+class NewsViewSet(viewsets.ModelViewSet):
+    queryset = News.objects.all()
+    serializer_class = NewsSerializer
+
