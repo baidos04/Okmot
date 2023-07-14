fd3da91a434e6677b93c5a661f42074974b972b7
erustan <131688989+erustan@users.noreply.github.com>
2023-07-15 05:16:30 +0600

    Удалены лишние классы в View. На оба модели: ContInfo, Feedback получены jsons

--- contactapp/views.py
+++ contactapp/views.py
@@ -17,35 +17,6 @@ class ContInfoListCreateView(generics.ListCreateAPIView):
     serializer_class = ContInfoSerializer
 
 
-# class ContInfoDetailView(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = ContInfo.objects.all()
-#     serializer_class = ContInfoSerializer
-
-
 class FeedbackListCreateView(generics.ListCreateAPIView):
     queryset = Feedback.objects.all()
     serializer_class = FeedbackSerializer
-
-
-# class FeedbackDetailView(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = Feedback.objects.all()
-#     serializer_class = FeedbackSerializer
-
-#
-# class FeedbackView(View):
-#     def get(self, request):
-#         form = FeedbackForm()
-#         return render(request, 'feedback.html', {'form': form})
-#
-#     def post(self, request):
-#         form = FeedbackForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('success')
-#         return render(request, 'feedback.html', {'form': form})
-#
-#
-# class SuccessView(View):
-#     def get(self, request):
-#         return render(request, 'success.html')
-
