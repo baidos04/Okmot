9d61ed1ccb2ddfd04aecccc4ba1add432438d550
baidos04 <muslimovbaidoskii@gmail.com>
2023-07-14 12:11:35 +0600

    pereshel na Baidosa 11234567

--- municipality/urls.py
+++ municipality/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from mainapp.urls import urlpatterns as mainapp_urls
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +27,7 @@ urlpatterns = [
     path('contact/', include('contactapp.urls')),
     path('about/', include('aboutapp.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
