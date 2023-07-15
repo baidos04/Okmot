a0f2eb8e337c67dcd1c858498013e4a290f42e5e
erustan <131688989+erustan@users.noreply.github.com>
2023-07-15 13:54:22 +0600

    del migrate and create new migrations

--- contactapp/urls.py
+++ contactapp/urls.py
@@ -1,18 +1,7 @@
 from django.urls import path
-from rest_framework import routers
-from django.contrib import admin
-
 from .views import *
-from . import views
-
-
-router = routers.DefaultRouter()
-router.register(r'contactapp', ContactappViewSet)
 
 urlpatterns = [
     path('contact-information/', ContInfoListCreateView.as_view(), name='contact-information-list'),
-    # path('contact-information/<int:pk>/', ContInfoDetailView.as_view(), name='contact-information-detail'),
     path('feedback/', FeedbackListCreateView.as_view(), name='feedback-list'),
-    # path('feedback/<int:pk>/', FeedbackDetailView.as_view(), name='feedback-detail'),
-    # path('success/', views.SuccessView.as_view(), name='success'),
 ]
